f2868b0a1ff8ad1ed3357a75a3b1865b8837ce9b
Seanxl <sean.liu@shownfun.com>
2018-06-28 21:04:37 +0800

    not enough memory

--- server/models/test.js
+++ server/models/test.js
@@ -2,10 +2,11 @@
 
 module.exports = function(Test) {
   Test.run = function(from, to, cb) {
-    const times = 1;
+    const times = 110;
     let updated = times;
     let cbCalled = false;
-    for (let i = times; i >= 0; i--) {
+    let ps = [];
+    for (let i = times; i > 0; i--) {
       // Test.findById(from)
       //   .then(fromUser => {
       //     if (fromUser.amount > 0) {
@@ -82,60 +83,143 @@ module.exports = function(Test) {
       // } finally {
       //   console.log('transaction end');
       // }
-      Test.beginTransaction({ isolationLevel: Test.Transaction.SERIALIZABLE, timeout: 30000 }, function(err, tx) {
-        if (err) {
-          if (!cbCalled) {
-            cbCalled = true;
-            return cb(err);
-          } else {
-            console.log(err);
-          }
+      // Test.beginTransaction({ isolationLevel: Test.Transaction.SERIALIZABLE, timeout: 30000 }, function(err, tx) {
+      //   if (err) {
+      //     if (!cbCalled) {
+      //       cbCalled = true;
+      //       return cb(err);
+      //     } else {
+      //       console.log(err);
+      //     }
+      //   }
+
+      //   tx.observe('timeout', function(context, next) {
+      //     console.log('tx timeout:', i);
+      //     next();
+      //   });
+
+      //   let options = { transaction: tx };
+
+      //   Test.findById(from, null, options)
+      //     .then(fromUser => {
+      //       console.log('fromUser:', fromUser);
+      //       if (fromUser.amount <= 0) {
+      //         throw new Error('not enough money');
+      //       }
+      //       return fromUser.updateAttributes({ amount: fromUser.amount - 1 }, options);
+      //     })
+      //     .then(fromUser => {
+      //       console.log('from:', fromUser.amount);
+      //       return Test.findById(to, null, options);
+      //     })
+      //     .then(toUser => {
+      //       return toUser.updateAttributes({ amount: toUser.amount + 1 }, options);
+      //     })
+      //     .then(toUser => {
+      //       tx.commit().then(() => {
+      //       }).catch(err => { throw err; });
+
+      //       console.log('to:', toUser.amount);
+      //       console.log('updated:', updated--);
+      //       if (updated == 0) {
+      //         cb(null, null);
+      //       }
+      //     })
+      //     .catch(err => {
+      //       tx.rollback().catch(err => { console.log(err); });
+
+      //       if (!cbCalled) {
+      //         cbCalled = true;
+      //         cb(err);
+      //       } else {
+      //         console.log(err);
+      //       }
+      //     });
+      // });
+      Test.doTransfer(from, to);
+    }
+    cb();
+  };
+
+  let sucCount = 0;
+
+  Test.doTransfer = function(from, to) {
+    let tx, fromUser, toUser;
+    let cbCalled = false;
+    return Promise.all([Test.findById(from), Test.findById(to)])
+      .then(res => {
+        [fromUser, toUser] = res;
+
+        // console.log('fromUser:', fromUser);
+        // console.log('toUser:', toUser);
+
+        if (!fromUser || !toUser) {
+          throw new Error('from or to id invalid');
         }
 
-        tx.observe('timeout', function(context, next) {
-          console.log('tx timeout:', i);
-          next();
-        });
+        if (fromUser.amount <= 0) {
+          throw new Error('not enough money');
+        }
 
+        return Test.beginTransaction({ isolationLevel: Test.Transaction.READ_COMMIT, timeout: 30000 });
+      })
+      .then(t => {
+        tx = t;
         let options = { transaction: tx };
 
-        Test.findById(from, null, options)
-          .then(fromUser => {
-            console.log('fromUser:', fromUser);
-            if (fromUser.amount <= 0) {
-              throw new Error('not enough money');
-            }
-            return fromUser.updateAttributes({ amount: fromUser.amount - 1 }, options);
-          })
-          .then(fromUser => {
-            console.log('from:', fromUser.amount);
-            return Test.findById(to, null, options);
-          })
-          .then(toUser => {
-            return toUser.updateAttributes({ amount: toUser.amount + 1 }, options);
-          })
-          .then(toUser => {
-            tx.commit().then(() => {
-            }).catch(err => { throw err; });
+        return Promise.all([
+          Test.updateAll({ amount: fromUser.amount, id: fromUser.id }, { amount: fromUser.amount - 1 }, options),
+          Test.updateAll({ amount: toUser.amount, id: toUser.id }, { amount: toUser.amount + 1 }, options),
+        ]);
+      })
+      .then(res => {
+        let [fromInfo, toInfo] = res;
+        console.log('updated:', fromInfo.count, toInfo.count);
+        if (fromInfo.count !== 1 || toInfo.count !== 1) {
+          let e = new Error('update faild');
+          e.retry = true;
+          throw e;
+        }
 
-            console.log('to:', toUser.amount);
-            console.log('updated:', updated--);
-            if (updated == 0) {
-              cb(null, null);
-            }
-          })
-          .catch(err => {
-            tx.rollback().catch(err => { console.log(err); });
+        if (tx) {
+          return tx.commit();
+        }
 
-            if (!cbCalled) {
-              cbCalled = true;
-              cb(err);
-            } else {
-              console.log(err);
-            }
-          });
+        console.log('success:', sucCount++);
+
+        //   return Test.findById(to);
+        // })
+        // .then(toUser => {
+        //   return Test.updateAll({ amount: toUser.amount, id: toUser.id }, { amount: toUser.amount + 1 });
+        // })
+        // .then(info => {
+        //   console.log('updated to:', info.count);
+        //   if (info.count !== 1) {
+        //     let e = new Error('update faild');
+        //     e.retry = true;
+        //     throw e;
+        //   }
+
+        //   return Test.findById(to);
+
+        //   console.log('to:', toUser.amount);
+        //   console.log('updated:', updated--);
+        //   if (updated == 0) {
+        //     cb(null, null);
+        //   }
+      })
+      .catch(err => {
+        if (tx) {
+          tx.rollback().catch(err => { console.log(err); });
+        }
+
+        if (err.retry) {
+          console.log('retrying...');
+          setTimeout(Test.doTransfer, 100, from, to);
+        }
+
+        console.log(err);
       });
-    }
   };
 
   Test.remoteMethod('run', {

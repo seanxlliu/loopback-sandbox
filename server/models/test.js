c8c198b757f12935c0c8a056607c9a06f8b36a6b
Seanxl <sean.liu@shownfun.com>
2018-06-28 10:26:10 +0800

    SERIALIZABLE deadlock

--- server/models/test.js
+++ server/models/test.js
@@ -2,42 +2,139 @@
 
 module.exports = function(Test) {
   Test.run = function(from, to, cb) {
-    const times = 110;
+    const times = 1;
     let updated = times;
     let cbCalled = false;
     for (let i = times; i >= 0; i--) {
-      Test.findById(from)
-        .then(fromUser => {
-          if (fromUser.amount > 0) {
-            fromUser.amount--;
-          } else {
-            throw new Error('not enough money');
-          }
-          return fromUser.save();
-        })
-        .then(fromUser => {
-          console.log('from:', fromUser.amount);
-          return Test.findById(to);
-        })
-        .then(toUser => {
-          toUser.amount++;
-          return toUser.save();
-        })
-        .then(toUser => {
-          console.log('to:', toUser.amount);
-          console.log('updated:', updated--);
-          if (updated == 0) {
-            cb(null, null);
-          }
-        })
-        .catch(err => {
+      // Test.findById(from)
+      //   .then(fromUser => {
+      //     if (fromUser.amount > 0) {
+      //       fromUser.amount--;
+      //     } else {
+      //       throw new Error('not enough money');
+      //     }
+      //     return fromUser.save();
+      //   })
+      //   .then(fromUser => {
+      //     console.log('from:', fromUser.amount);
+      //     return Test.findById(to);
+      //   })
+      //   .then(toUser => {
+      //     toUser.amount++;
+      //     return toUser.save();
+      //   })
+      //   .then(toUser => {
+      //     console.log('to:', toUser.amount);
+      //     console.log('updated:', updated--);
+      //     if (updated == 0) {
+      //       return (null, null);
+      //     }
+      //   })
+      //   .catch(err => {
+      //     if (!cbCalled) {
+      //       cbCalled = true;
+      //       return err;
+      //     } else {
+      //       console.log(err);
+      //     }
+      //   });
+      // try {
+      //   await Test.app.dataSources.db.transaction(async models => {
+      //     console.log('transaction begin');
+
+      //     const { Test } = models;
+
+      //     Test.findById(from)
+      //       .then(fromUser => {
+      //         if (fromUser.amount > 0) {
+      //           fromUser.amount--;
+      //         } else {
+      //           throw new Error('not enough money');
+      //         }
+      //         return fromUser.save();
+      //       })
+      //       .then(fromUser => {
+      //         console.log('from:', fromUser.amount);
+      //         return Test.findById(to);
+      //       })
+      //       .then(toUser => {
+      //         toUser.amount++;
+      //         return toUser.save();
+      //       })
+      //       .then(toUser => {
+      //         console.log('to:', toUser.amount);
+      //         console.log('updated:', updated--);
+      //         if (updated == 0) {
+      //           return (null, null);
+      //         }
+      //       })
+      //       .catch(err => {
+      //         if (!cbCalled) {
+      //           cbCalled = true;
+      //           return err;
+      //         } else {
+      //           console.log(err);
+      //         }
+      //       });
+      //   });
+      // } catch (e) {
+      //   console.log(e);
+      // } finally {
+      //   console.log('transaction end');
+      // }
+      Test.beginTransaction({ isolationLevel: Test.Transaction.SERIALIZABLE, timeout: 30000 }, function(err, tx) {
+        if (err) {
           if (!cbCalled) {
             cbCalled = true;
-            cb(err);
+            return cb(err);
           } else {
             console.log(err);
           }
+        }
+
+        tx.observe('timeout', function(context, next) {
+          console.log('tx timeout:', i);
+          next();
         });
+
+        let options = { transaction: tx };
+
+        Test.findById(from, null, options)
+          .then(fromUser => {
+            console.log('fromUser:', fromUser);
+            if (fromUser.amount <= 0) {
+              throw new Error('not enough money');
+            }
+            return fromUser.updateAttributes({ amount: fromUser.amount - 1 }, options);
+          })
+          .then(fromUser => {
+            console.log('from:', fromUser.amount);
+            return Test.findById(to, null, options);
+          })
+          .then(toUser => {
+            return toUser.updateAttributes({ amount: toUser.amount + 1 }, options);
+          })
+          .then(toUser => {
+            tx.commit().then(() => {
+            }).catch(err => { throw err; });
+
+            console.log('to:', toUser.amount);
+            console.log('updated:', updated--);
+            if (updated == 0) {
+              cb(null, null);
+            }
+          })
+          .catch(err => {
+            tx.rollback().catch(err => { console.log(err); });
+
+            if (!cbCalled) {
+              cbCalled = true;
+              cb(err);
+            } else {
+              console.log(err);
+            }
+          });
+      });
     }
   };
 
